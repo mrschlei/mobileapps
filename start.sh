a242b2b750d3f930f9ac3e992834562d99f93490
Ben Fairfield <benfa@umich.edu>
2018-02-13 13:31:27 -0500

    start.sh has nt been deployed. deleted blank line

--- start.sh
+++ start.sh
@@ -16,8 +16,27 @@ ln -sf /secrets/app/settings.php /var/www/html/sites/default/settings.php
 ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
 ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
 ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
-ln -sf /secrets/ssl/its-mobileapps.openshift.dsc.umich.edu.cert /etc/ssl/certs/its-mobileapps.openshift.dsc.umich.edu.cert
-ln -sf /secrets/ssl/its-mobileapps.openshift.dsc.umich.edu.key /etc/ssl/private/its-mobileapps.openshift.dsc.umich.edu.key
+
+# If it exists, include local.start.sh
+[ -f /secrets/start/local.start.sh ] && /secrets/start/local.start.sh
+
+# These lines would need to be in a site-specific local.start.sh
+##### begin local.start.sh
+#ln -sf /secrets/ssl/its-mobileapps.openshift.dsc.umich.edu.cert /etc/ssl/certs/its-mobileapps.openshift.dsc.umich.edu.cert
+#ln -sf /secrets/ssl/its-mobileapps.openshift.dsc.umich.edu.key /etc/ssl/private/its-mobileapps.openshift.dsc.umich.edu.key
+
+#mkdir /var/www/html/sites/documentation.its.openshift.dsc.umich.edu
+#mkdir /var/www/html/sites/its-announce.openshift.dsc.umich.edu
+#mkdir /var/www/html/sites/its-content.openshift.dsc.umich.edu
+#mkdir /var/www/html/sites/itservices-drupal-dev.it.openshift.dsc.umich.edu
+#mkdir /var/www/html/sites/wolvaccessannounce-drupal.openshift.dsc.umich.edu
+
+#ln -sf /secrets/app/documentation.its.openshift.dsc.umich.edu.settings.php /var/www/html/sites/documentation.its.openshift.dsc.umich.edu/settings.php
+#ln -sf /secrets/app/its-announce.openshift.dsc.umich.edu.settings.php /var/www/html/sites/its-announce.openshift.dsc.umich.edu/settings.php
+#ln -sf /secrets/app/its-content.openshift.dsc.umich.edu.settings.php /var/www/html/sites/its-content.openshift.dsc.umich.edu/settings.php
+#ln -sf /secrets/app/itservices-drupal-dev.it.openshift.dsc.umich.edu.settings.php /var/www/html/sites/itservices-drupal-dev.it.openshift.dsc.umich.edu/settings.php
+#ln -sf /secrets/app/wolvaccessannounce-drupal.openshift.dsc.umich.edu/settings.php /var/www/html/sites/wolvaccessannounce-drupal.openshift.dsc.umich.edu/settings.php
+##### end local.start.sh
 
 ## Rehash command needs to be run before starting apache.
 c_rehash /etc/ssl/certs
@@ -25,18 +44,6 @@ c_rehash /etc/ssl/certs
 a2enmod ssl
 a2enmod include
 a2ensite default-ssl 
-
-mkdir /var/www/html/sites/documentation.its.openshift.dsc.umich.edu
-mkdir /var/www/html/sites/its-announce.openshift.dsc.umich.edu
-mkdir /var/www/html/sites/its-content.openshift.dsc.umich.edu
-mkdir /var/www/html/sites/itservices-drupal-dev.it.openshift.dsc.umich.edu
-mkdir /var/www/html/sites/wolvaccessannounce-drupal.openshift.dsc.umich.edu
-
-ln -sf /secrets/app/documentation.its.openshift.dsc.umich.edu.settings.php /var/www/html/sites/documentation.its.openshift.dsc.umich.edu/settings.php
-ln -sf /secrets/app/its-announce.openshift.dsc.umich.edu.settings.php /var/www/html/sites/its-announce.openshift.dsc.umich.edu/settings.php
-ln -sf /secrets/app/its-content.openshift.dsc.umich.edu.settings.php /var/www/html/sites/its-content.openshift.dsc.umich.edu/settings.php
-ln -sf /secrets/app/itservices-drupal-dev.it.openshift.dsc.umich.edu.settings.php /var/www/html/sites/itservices-drupal-dev.it.openshift.dsc.umich.edu/settings.php
-ln -sf /secrets/app/wolvaccessannounce-drupal.openshift.dsc.umich.edu/settings.php /var/www/html/sites/wolvaccessannounce-drupal.openshift.dsc.umich.edu/settings.php
 
 ## set SGID for www-data 
 chown -R www-data.www-data /var/www/html /var/cosign
